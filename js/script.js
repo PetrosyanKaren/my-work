632c52d289bb0debaa6246a89996762aa6759058
PetrosyanKaren <petrosyankaren926@gmail.com>
2022-08-29 21:07:09 +0400

    main

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// var str = 'aaa@bbb@ccc';
+// console.log(str.replace('@','!'));    //poxarinum e arajin handipac @y  '@'
+
+// var str = 'aaa@bbb@ccc';
+// console.log(str.replace(/@/g,'!'));   //poxarinum e bolor @nery  /@/g
+
+// var str = 'aaa bbb ccc';
+// console.log(str.substr(4,3));    //4rd indexov tarric sksac aydqan hat
+
+// var str ='aaabbbccc';
+// console.log(str.substring[1,3));
+
+// var str = '01-12-2018';
+// console.log(str.split('-'));
+
+// var arr = [1,2,3,4,5];
+// arr.push(7);   //verjic avelacnum e 
+// arr.pop();    //verjic hanum e 
+// arr.pop();
+// console.log(arr)
+
+
+// var arr = [1,2,3,4,5];
+// arr.unshift(9);   //skzbic avelacnum e
+// arr.unshift(8);
+// console.log(arr);
+
+// var arr = [1,2,3,4,5,6,7];
+// arr.shift()   //skzbic ktrum hanum e
+// arr.shift()
+// console.log(arr)
+
+// var arr= [6,1,2,4,3,5];
+// arr.sort()
+// console.log(arr)
+
+// var str = 'hello';
+// str = str[0].toUpperCase() + str.substr(1);   //texti arajin tary sarqum e mecatar
+// console.log(str)
+
+// var x = 'hayk abelyan';
+// var res = x.split(' ');
+// console.log(res[0][0].toUpperCase() + res[0].substr(1));
+// console.log(res[0][1].toUpperCase() + res[1].substr(1)); //araqin zangvacyi tarery arancin avelacnum e erkrord zangvaci mech ev mecatar e sarqum arajin tarery
+
+// var str = 'hello';
+// str = str.split('');
+// str[0]= str[0].toUpperCase();
+// var result = str.join('');
+// console.log(result);  //texty sarqecinq zangvac arajin tary sarqecinq mecatar eli sarqecinq text
+
+// var str = 'http://phphtml';
+// if(str.substr(0,7) == 'http://'){
+//     console.log('yes')
+// } else{
+//     console.log('no')
+// } //stugum e grvacy link e te voch
+
+
+// var str = 'index.html';
+// if(str.substr(-5) == '.html'){
+//     console.log('yes')
+// } else{
+//     console.log('no')
+// }//ete verjic 5 haty hamapatasxanum e stugmany tpum e yes
+
